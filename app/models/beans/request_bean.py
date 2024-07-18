101a3127ded3837e886fa37eac1fd9d20d7e06ba
Suriya-Veeran <suriya.veeran@platform3solutions.com>
2024-07-18 16:09:10 +0530

    Bean Value Added

---
+++ app/models/beans/request_bean.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+# ------------------------------------------------------------------------------------------
+class Position(BaseModel):
+    x: int
+    y: int
+    width: int
+    height: int
+
+
+class ImageDetails(BaseModel):
+    image_content: str
+    image_name: str
+
+
+class FieldDetails(BaseModel):
+    binding_name: str
+    position: Position
+
+
+class RequestBean(BaseModel):
+    template_name: str
+    image_details: List[ImageDetails]
+    field_details: List[FieldDetails]
+
+
+# ------------------------------------------------------------------------------------------
