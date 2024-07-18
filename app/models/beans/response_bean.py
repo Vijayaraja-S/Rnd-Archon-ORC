7ab6dee1c171d03959a87637e7b71809a577048a
Suriya-Veeran <suriya.veeran@platform3solutions.com>
2024-07-18 19:15:07 +0530

    response bean added

---
+++ app/models/beans/response_bean.py
@@ -0,0 +1,42 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class DocumentTypeInfo(BaseModel):
+    id: str
+    template_name: str
+
+
+class DocumentInfoResponseBean(BaseModel):
+    response: List[DocumentTypeInfo]
+
+
+class DocumentInfo(BaseModel):
+    id: str
+    image_name: str
+    image_content: str
+    createdAt: str
+    modifiedAt: str
+    status: str
+
+
+class DocumentResponse(BaseModel):
+    document_info: List[DocumentInfo]
+
+
+class Position(BaseModel):
+    x: int
+    y: int
+    width: int
+    height: int
+
+
+class FieldInfo(BaseModel):
+    bindingName: str
+    position: Position
+    value: str
+    accuracy: str
+
+
+class FieldInfoResponseBean(BaseModel):
+    response: List[FieldInfo]
